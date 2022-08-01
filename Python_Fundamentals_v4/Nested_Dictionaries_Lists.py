810c89f7ffbbada6b0eb43a0984db517f6bee078
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-01 16:42:57 -0700

    initial commit

--- Python_Fundamentals_v4/Nested_Dictionaries_Lists.py
+++ Python_Fundamentals_v4/Nested_Dictionaries_Lists.py
@@ -1,22 +1,22 @@
-# # Update Values in Dictionaries and Lists
-# x = [ [5,2,3], [10,8,9] ] 
-# students = [
-#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#      {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# z = [ {'x': 10, 'y': 20} ]
+# Update Values in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
 
-# x[1][0] = 15
-# students[0]['last_name'] = 'Bryant'
-# sports_directory['soccer'][0] = 'Andres'
-# z[0]['y'] = 30
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
 
-# print(students)
-# print(sports_directory)
+print(students)
+print(sports_directory)
 
 
 # Iterate Through a List of Dictionaries
@@ -33,36 +33,36 @@ def iterateDictionary(studentsList):
 iterateDictionary(students)
 
 
-# # # should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# # # bonus to get them to appear exactly as below!)
-# # # first_name - Michael, last_name - Jordan
-# # # first_name - John, last_name - Rosales
-# # # first_name - Mark, last_name - Guillen
-# # # first_name - KB, last_name - Tonel
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
 
-# # Get Values From a List of Dictionaries
-# def iterateDictionary2(key_name, some_list):
-#     for x in range(len(some_list)):
-#         print(some_list[x][key_name])
+# Get Values From a List of Dictionaries
+def iterateDictionary2(key_name, some_list):
+    for x in range(len(some_list)):
+        print(some_list[x][key_name])
 
-# iterateDictionary2('first_name', students)
-# iterateDictionary2('last_name', students)
-
-
-# # Iterate Through a Dictionary with List Values
-# dojo = {
-#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-# }
-
-# def printInfo(some_dict):
-#     for key in some_dict:
-#         print(len(some_dict[key]), str(key))
-#         for keys in range(len(some_dict[key])):
-#             print(some_dict[key][keys])
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
 
 
-# printInfo(dojo)
+# Iterate Through a Dictionary with List Values
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key in some_dict:
+        print(len(some_dict[key]), str(key))
+        for keys in range(len(some_dict[key])):
+            print(some_dict[key][keys])
+
+
+printInfo(dojo)
 
 
