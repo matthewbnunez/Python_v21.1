f7ce18029a7a5bc3c166f74909819a8d49ff5e1a
Matthew Nunez <matthewbnunez@.com>
2022-08-01 10:41:15 -0700

    initial commit

--- Python_Fundamentals_v4/For_Loops_Basic_I.py
+++ Python_Fundamentals_v4/For_Loops_Basic_I.py
@@ -1,9 +1,12 @@
+# Basic
 for x in range(0, 151):
     print(x)
 
+# Multiples of Five
 for x in range(5, 1001, 5):
     print(x)
 
+# Counting, the Dojo Way
 for x in range(1, 101):
     if x % 10 == 0:
         print("Coding Dojo")
@@ -12,15 +15,18 @@ for x in range(1, 101):
     else:
         print(x)
 
+# Whoa. That Sucker's Huge
 sum = 0
 for x in range(0, 500000):
     if x % 2 != 0:
         sum = sum + x
 print(sum)
 
+# Countdown by Fours 
 for x in range(2018, 0, -4):
     print(x)
 
+# Flexible Counter 
 lowNum = 2
 highNum = 9
 mult = 3
