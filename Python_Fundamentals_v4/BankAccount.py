8de2995c7644579ad3ce536fd28e2213beb8fdc8
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-02 13:23:57 -0700

    initial commit

--- Python_Fundamentals_v4/BankAccount.py
+++ Python_Fundamentals_v4/BankAccount.py
@@ -1,10 +1,17 @@
 class BankAccount:
     # don't forget to add some default values for these parameters!
+    all_accounts = []
     def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.all_accounts.append(self)
         # your code here! (remember, instance attributes go here)
         # don't worry about user info here; we'll involve the User class soon
+    @classmethod
+    def print_all_accounts(cls):
+        for one_account in cls.all_accounts:
+            print(f'Interest Rate: {one_account.int_rate} Balance: {one_account.balance}')
+
     def deposit(self, amount):
         print(f'You are depositing ${amount} in your account')
         self.balance += amount
@@ -30,3 +37,5 @@ Bob = BankAccount(.12, 150)
 
 Jake.deposit(200).deposit(300).deposit(150).withdraw(50).display_account_info().yield_interest()
 Mike.deposit(13000).deposit(8000).withdraw(5000).withdraw(1000).withdraw(4000).withdraw(50000).display_account_info().yield_interest()
+
+BankAccount.print_all_accounts()
