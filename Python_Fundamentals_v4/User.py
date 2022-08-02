59a144f460d711a4a3bca193cfe1db4c15449d3e
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-02 10:11:28 -0700

    initial commit

--- Python_Fundamentals_v4/User.py
+++ Python_Fundamentals_v4/User.py
@@ -6,39 +6,42 @@ class User:
         self.age = age
         self.is_reward_member = False
         self.gold_card_points = gold_card_points
+
     
     def display_info(self):
         print("==========================")
         print(self.first_name, self.last_name, self.email, self.age, self.is_reward_member, self.gold_card_points)
         print("==========================")
+        return self
 
 
     def enroll(self):
         if(self.is_reward_member != True):
+            print(f'{self.first_name} {self.last_name} is now enrolled')
             self.is_reward_member = True
             self.gold_card_points = 200
         else:
             print(f'{self.first_name} {self.last_name} is already enrolled')
+        return self
+
 
     def spend_points(self, amount):
         if(self.gold_card_points >= amount):
+            print("==========================")
             print(f'{self.first_name} {self.last_name} spent {amount} points')
             self.gold_card_points -= amount
             print(f'and now has {self.gold_card_points} points left')
+            print("==========================")
         else:
+            print("==========================")
             print(f'{self.first_name} {self.last_name} does not have enough points')
+        return self
+
 
 Matthew = User('Matthew', 'Nunez', 'gmail', 23, 0)
 Jim = User('Jim', 'Brown', 'hotmail', 63, 0)
 Alex = User('Alex', 'White', 'Yahoomail', 43, 40)
 
-User.enroll(Matthew)
-Matthew.spend_points(150)
-User.enroll(Jim)
-Jim.spend_points(80)
-User.enroll(Matthew)
-Alex.spend_points(450)
-
-User.display_info(Matthew)
-User.display_info(Jim)
-User.display_info(Alex)
+Matthew.enroll().spend_points(150).enroll().display_info()
+Jim.enroll().spend_points(80).display_info()
+Alex.spend_points(450).display_info()
