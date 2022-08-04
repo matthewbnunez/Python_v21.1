6ed11ad5fee9b47b184706f370402eeddcfe9f8f
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-04 09:38:20 -0700

    initial commit

--- Flask/PlayGround/server.py
+++ Flask/PlayGround/server.py
@@ -18,12 +18,6 @@ def level2(num):
 def level3(color, num):
     return render_template('index.html', num = num, color = color)
 
-@app.route('/repeat/<num>/<text>') 
-def show_user_profile(num, text):
-    return int(num) * str(text)
-
-
-
 # app.run(debug=True) should be the very last statement! 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
