46816d524736776a45cc832e530af6093b6c0f34
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-04 20:59:48 -0700

    initial commit

--- Flask/CheckerBoard/server.py
+++ Flask/CheckerBoard/server.py
@@ -4,24 +4,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def level1():
     return render_template("index.html", row = 8, col = 8, color1 = 'red', color2 = 'black')
 
-
 @app.route('/<int:x>')
-def row(row):
+def level2(row):
     return render_template("index.html", row = row, col = 8, color1 = 'red', color2 = 'black')
 
 @app.route('/<int:x>/<int:y>')
-def row_col(row, col):
+def level3(row, col):
     return render_template("index.html", row = row, col = col, color1 = 'red', color2 = 'black')
 
 @app.route('/<int:x>/<int:y>/<color1>')
-def row_col_one(row, col, color1):
+def level4(row, col, color1):
     return render_template("index.html", row = row, col = col, color1 = color1, color2 = 'black')
 
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
-def row_col_two(row, col, color1, color2):
+def level5(row, col, color1, color2):
     return render_template("index.html", row = row ,col = col ,color1 = color1, color2 = color2)
 
 if __name__=="__main__":
