b98ec907b96bd212dda3ef5cf656917db3dcb10b
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-04 17:10:27 -0700

    initial commit

---
+++ Flask/HTML_Table/server.py
@@ -0,0 +1,39 @@
+# Import Flask to allow us to create our app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/success')
+def success():
+    return "success <h1>this is a h1</h1><script>alert('hello!')</script>"
+
+
+@app.route('/hello/<name>/<int:times>')
+def hello(name, times):
+    return render_template("jinja.html", name=name, times=times)
+
+
+@app.route('/template')
+def template():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+    return render_template("index.html", users=users)
+
+
+@app.errorhandler(404)
+def error_page(e):
+    return "my custom error page"
+
+
+if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode.
