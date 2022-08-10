72365137577bc3be4e1d1368a385c2061d100837
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-10 10:52:17 -0700

    initial commit

---
+++ Algos/W2D3.js
@@ -0,0 +1,134 @@
+/* 
+Parens Valid
+Given an str that has parenthesis in it
+return whether the parenthesis are valid
+*/
+
+const str1 = "Y(3(p)p(3)r)s"; // ( () () )
+const expected1 = true;
+
+const str2 = "N(0(p)3"; // ( ( ) 
+const expected2 = false;
+// Explanation: not every parenthesis is closed.
+
+const str3 = "N(0)t ) 0(k"; // ( ) ) (
+const expected3 = false;
+// Explanation: because the second ")" is premature: there is nothing open for it to close.
+
+
+/**
+ * Determines whether the parenthesis in the given string are valid.
+ * Each opening parenthesis must have exactly one closing parenthesis.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the parenthesis are valid.
+ */
+function parensValid(str) {
+    count = 0;
+    for (i = 0; i < str.length; i++) {
+        if (str[i] == '(') {
+            count++;
+        }
+        else if (str[i] == ')') {
+            count--;
+            if (count < 0) {
+                return false;
+            }
+        }
+    }
+    if (count == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(parensValid(str1)) // expected: true
+console.log(parensValid(str2)) // expected: false
+console.log(parensValid(str3)) // expected: false
+
+
+/* 
+Braces Valid
+Given a string sequence of parentheses, braces and brackets, determine whether it is valid. 
+*/
+
+const strA = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
+const expectedA = true;
+
+const strB = "D(i{a}l[ t]o)n{e";
+const expectedB = false;
+
+const strC = "A(1)s[O (n]0{t) 0}k";
+const expectedC = false;
+
+/**
+ * Determines whether the string's braces, brackets, and parenthesis are valid
+ * based on the order and amount of opening and closing pairs.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the given strings braces are valid.
+ */
+function bracesValid(str) {
+    const stack = [];
+    const isOpen = { "(": true, "{": true, "[": true }
+    const closesToOpens = { ")": "(", "}": "{", "]": "[" };
+
+    for (let char of str) {
+        if (isOpen[char]) {
+            stack.push(char);
+        } else if (closesToOpens[char]) {
+            if (closesToOpens[char] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+
+    // -----My Solution-----
+    //
+    // parenthesis = 0;
+    // brackets = 0;
+    // square = 0
+    // for(i =0; i < str.length; i++) {
+    //     if (str[i] == '(' ) {
+    //         parenthesis++;
+    //     }
+    //     else if (str[i] == ')'){
+    //         parenthesis--;
+    //         if (parenthesis < 0){
+    //             return false;
+    //         }
+    //     }
+    //     if (str[i] == '{' ) {
+    //         brackets++;
+    //     }
+    //     else if (str[i] == '}'){
+    //         brackets--;
+    //         if (brackets < 0){
+    //             return false;
+    //         }
+    //     }
+    //     if (str[i] == '[' ) {
+    //         square++;
+    //     }
+    //     else if (str[i] == ']'){
+    //         square--;
+    //         if (square < 0){
+    //             return false;
+    //         }
+    //     }
+    // }
+    // if(parenthesis == 0 && brackets == 0 && square == 0){
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+}
+console.log(bracesValid(strA)) // expected: true
+console.log(bracesValid(strB)) // expected: false
+console.log(bracesValid(strC)) // expected: false
